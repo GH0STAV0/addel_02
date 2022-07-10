44aeb533743ef7ffbbdacfcb0637688dde7f35db
GH0STAV0 <you@example.com>
2022-07-10 20:10:59 +0100

    my connecto python

--- save_hamster.py
+++ save_hamster.py
@@ -16,7 +16,9 @@ database="f6V3kVwxvH"
 
 
 def insert_to_db(cnf_name):
-	mydb = mysql.connector.connect(host="remotemysql.com",user="f6V3kVwxvH",passwd="sOVnW1130i",database="f6V3kVwxvH")
+	mydb = mysql.connector.connect(host="sql8.freesqldatabase.com",user="sql8505358",passwd="ILiAHKV4bs",database="sql8505358")
+	# mydb = mysql.connector.connect(host="remotemysql.com",user="f6V3kVwxvH",passwd="sOVnW1130i",database="f6V3kVwxvH")
+
 	mycursor = mydb.cursor()
 	sql = "INSERT INTO live_x (u_name, u_pass,status_actv) VALUES (%s, %s, %s)"
 	val = (cnf_name, "baba123A*","NA")
